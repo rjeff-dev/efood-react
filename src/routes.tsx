6b51990934464244c446fe740e812418a4390aeb
rjeff-dev <jeff_xd00@hotmail.com>
2026-06-29 12:26:50 -0300

    Last

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import Perfil from './pages/Perfil'
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/categories" element={<Perfil />} />
+    <Route path="/perfil" element={<Perfil />} />
   </Routes>
 )
 
